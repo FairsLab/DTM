f5e3e4f5900d76fe39cbe44ae4eda647fd1a8a49
tc2000731 <tc2000731@163.com>
2023-12-29 04:07:58 +0800

    规范代码 加了些注释

--- data_trade/traci_calculation.py
+++ data_trade/traci_calculation.py
@@ -19,9 +19,8 @@ def Update_Cars_info(global_context):
 
 def Calc_traffic_flow(global_context):
     vids = traci.vehicle.getIDList()
-    # print(vids)
 
-    for vid in vids:
+    for vid in vids:  # 这个地方还可以优化 但比较麻烦
         res = {}
         if global_context["vehicles"][vid].vtype == "human":
             continue
@@ -45,19 +44,21 @@ def Calc_nearby_accident(global_context):
         for eid in global_context["event"].accident_object.keys():
             if eid in global_context["vehicles"][vid].accident.keys():
                 continue
-            a = np.array(global_context["vehicles"][vid].position)
-            b = np.array(global_context["event"].accident_position[eid])
-            if np.linalg.norm(a-b) < global_context["range"]:
+            vehicle_position = np.array(
+                global_context["vehicles"][vid].position)
+            event_position = np.array(
+                global_context["event"].accident_position[eid])
+            if np.linalg.norm(event_position-vehicle_position) < global_context["range"]:
                 global_context["vehicles"][vid].accident[eid] = {
                     "Accident_Location": global_context["event"].accident_location,
-                    "Accident_Position": b,
+                    "Accident_Position": event_position,
                     "accident_time": global_context["step"],  # detect time
                     "accident_vid": global_context["event"].accident_object[eid],
                     "traded": False
                 }
 
 
-def start_trade(global_context):
+def start_trade(global_context):  # 判断哪些车辆需要进入交易(距离下一个信号灯足够近)
     vids = traci.vehicle.getIDList()
     for vid in vids:
         if traci.vehicle.getTypeID(vid) == "human":
@@ -70,9 +71,9 @@ class SumoVehicle:
     def __init__(self, car_id, init_currancy, vtype):
         self.vtype = vtype
         self.car_id = car_id
-        self.history_trading_data = []
+        self.accident = {}  # 目击到且尚未交易的事故
+        self.history_trading_data = []  # 交易完成后把交易记录按顺序存储在里面
         self.currancy = init_currancy
-        self.accident = {}
 
     def update_info(self, step):
         self.update_step = step
@@ -90,22 +91,27 @@ class SumoVehicle:
             self.traffic_flow[key]["visible_vehicle_count"].append(
                 traffic_flow[key])
 
-    def get_trading_data(self, global_context):
+    def get_trading_data(self, global_context: dict):
         if len(self.accident) == 0:
-            return None
+            return None  # 如果没有可以交易的事故就返回None
         accident_id = list(self.accident.keys())[0]
 
-        accident_info = {"Accident_id": self.accident[accident_id],
-                         "Accident_Location": self.accident[accident_id]["Accident_Location"],
-                         "Time_To_Trading_Point": str((global_context["step"]-self.accident[accident_id]["accident_time"])/10)+"s",
-                         "Distance_To_Trading_Point": str(np.linalg.norm(self.accident[accident_id]["Accident_Position"]-np.array(self.position)))+"m",
-                         "Accident_Severity": "high",
-                         "Traffic_Flow": self.traffic_flow,
-                         }
+        # 按照trading_test中的格式, 多了一个Accident_id方便以后要用
+        accident_info = {
+            "Accident_Id": self.accident[accident_id],
+            "Accident_Location": self.accident[accident_id]["Accident_Location"],
+            "Time_To_Trading_Point": str((global_context["step"]-self.accident[accident_id]["accident_time"])/10)+"s",
+            "Distance_To_Trading_Point": str(np.linalg.norm(self.accident[accident_id]["Accident_Position"]-np.array(self.position)))+"m",
+            "Accident_Severity": "high",
+            # 目前是一个字典, 键是道路ID ,值是一个list表示每个观测时刻看到的车辆数(每次进入新的道路刷新)
+            "Traffic_Flow": self.traffic_flow,
+        }
+        # 因为还没设定好交易完成后的数据格式, 这里历史平均价格本来应该从history_trading_data计算出来的
+        # 还有就是如果当前是第一次交易那平均价格应该也是None才对
         return TradingData(current_token=self.currancy,
                            history_average_price=None, trading_history=self.history_trading_data, accident_info=accident_info)
 
-    def get_personal_data(self, global_context):
+    def get_personal_data(self, global_context: dict):
         return PersonalData(self.position)
 
 
