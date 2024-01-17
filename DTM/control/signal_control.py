d0660e96807bab2e61817eff1c4fa20b9dc58e3c
yuyi <512154936@qq.com>
2024-01-17 18:10:45 +0800

    signal control comments

--- DTM/control/signal_control.py
+++ DTM/control/signal_control.py
@@ -1,3 +1,21 @@
 class SignalControl:
     def __init__(self):
         pass
+#     a=random.rand(1)
+#     if change_rate < a:
+#         traci.trafficlight.setPhaseId(id, p1)
+            
+    
+
+
+# phase('rrrgggrrrr')ss
+# road_id
+# if current_phase == r:
+    
+#     another_pahse_plan={'rrr
+#                         ggg
+#                         ggg
+#                         rrr
+#                         ',id = p2}
+#     traci.trafficlight.setPhaseId = p2
+#     s
